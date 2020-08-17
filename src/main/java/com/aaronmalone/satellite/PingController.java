8af239bfd51bb9c9f5a364e00863701076fb11d7
Aaron Malone <aaronmalone@gmail.com>
2020-08-16 20:18:12 -0400

    Added ping endpoint

---
+++ src/main/java/com/aaronmalone/satellite/PingController.java
@@ -0,0 +1,12 @@
+package com.aaronmalone.satellite;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+  @GetMapping(path = "/ping")
+  public String ping() {
+    return "Hello!";
+  }
+}
