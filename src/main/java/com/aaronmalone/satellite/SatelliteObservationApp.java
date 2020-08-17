0698b9b5f1d20db62f957c7132c5ae6f22e43421
Aaron Malone <aaronmalone@gmail.com>
2020-08-16 20:14:21 -0400

    Initial commit of bootstrapped project

---
+++ src/main/java/com/aaronmalone/satellite/SatelliteObservationApp.java
@@ -0,0 +1,13 @@
+package com.aaronmalone.satellite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatelliteObservationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatelliteObservationApp.class, args);
+	}
+
+}
